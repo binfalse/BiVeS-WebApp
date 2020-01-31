040337778f84adddd34872b90c1ddbc7baac1a3d
Tom Gebhardt <tom.gebhardt@uni-rostock.de>
2020-01-31 14:27:53 +0100

    martin fixed string test

--- src/test/java/de/unirostock/sems/bives/webservice/TestWeb.java
+++ src/test/java/de/unirostock/sems/bives/webservice/TestWeb.java
@@ -13,7 +13,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
@@ -301,10 +303,11 @@ public class TestWeb
 		StringBuilder file = new StringBuilder ();
 		try
 		{
-			BufferedReader br = new BufferedReader(new InputStreamReader(new BufferedInputStream(new URL(FILE1).openStream())));
-			while (br.ready ())
+			BufferedReader br = new BufferedReader(new InputStreamReader(new URL(FILE1).openStream()));
+	        String inputLine;
+			while ((inputLine = br.readLine()) != null)
 			{
-				file.append (br.readLine () + "\n");
+				file.append (inputLine + "\n");
 			}
 			br.close ();
 		}
